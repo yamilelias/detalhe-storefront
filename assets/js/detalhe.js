e0ad64d372be4e3552701384b7f104cb1bfcd389
yamilelias <yamileliassoto@gmail.com>
2017-11-17 12:47:05 -0700

    Finished styling for search bar

--- assets/js/detalhe.js
+++ assets/js/detalhe.js
@@ -2,10 +2,6 @@
  * Created by yamilelias on 16/11/17.
  */
 
-function openSearch() {
-    var ob = $("#woocommerce-product-search-field-0");
-    console.log("CSS: " + JSON.stringify(ob));
-}
 $(document).ready(function(){
     // for search bar
     $(".search-icon").click(function(){
