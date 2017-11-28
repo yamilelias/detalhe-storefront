4c29f0eef7a720f47a90c91440243afc11f36f84
yamilelias <yamileliassoto@gmail.com>
2017-11-28 10:50:06 -0700

    brands page responsiveness

--- inc/detalhe/detalhe-template-functions.php
+++ inc/detalhe/detalhe-template-functions.php
@@ -39,32 +39,26 @@ if ( ! function_exists( 'detalhe_primary_navigation' ) ) {
      * @return void
      */
     function detalhe_primary_navigation() {
-        $logo_item = '<div class="navbar-header">' .
-                        '<div class="navbar-header">' .
-                            '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-items" aria-expanded="false">' .
-                                    '<i class="fa fa-bars" aria-hidden="true"></i>' .
-                            '</button>' .
-                            '<a class="navbar-brand" href="' . get_permalink( wc_get_page_id( 'shop' ) ) .'">'
-                                . detalhe_site_title_or_logo(false) . // So it won't echo and return the item
-                            '</a>' .
-                        '</div>' .
-                    '</div>';
-        ?>
+    ?>
         <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_html_e( 'Primary Navigation', 'storefront' ); ?>">
+            <div class="navbar-header">
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-items" aria-expanded="false">
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </button>
+                    <a class="navbar-brand" href="<?php get_permalink( wc_get_page_id( 'shop' ) ) ?>">
+                        <?php detalhe_site_title_or_logo(); // So it won't echo and return the item ?>
+                    </a>
+                </div>
+            </div>
+
             <?php
             wp_nav_menu(
                 array(
                     'theme_location'	=> 'primary',
                     'container_class'	=> 'primary-navigation container-fluid',
-                    'items_wrap'        => $logo_item .'<ul class="main-menu">%3$s</ul>',
-                )
-            );
-
-            wp_nav_menu(
-                array(
-                    'theme_location'	=> 'handheld',
-                    'container_class'	=> 'handheld-navigation container-fluid',
-                    'items_wrap'        => $logo_item .'<ul class="main-menu">%3$s</ul>',
+                    'container_id'      => 'menu-items',
+                    'items_wrap'        => '<ul class="main-menu">%3$s</ul>',
                 )
             );
             ?>
